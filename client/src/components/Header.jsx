97541a58cf3a96b1f597ca5408e782f61886cdaf
Rahul vinod <rahulvinod135@gmail.com>
2025-07-21 12:00:59 +0530

    feat: api integration - logout

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,15 +3,17 @@ import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 
 const Header = () => {
-  const {
-    userData: { name },
-  } = useContext(AppContext);
+  const { userData } = useContext(AppContext);
 
   return (
     <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800">
       <img src={assets.header_img} className="w-36 h-36 rounded-full mb-6" />
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
-        Hey {name ? name : "Developer"}!
+        Hey{" "}
+        {userData?.name
+          ? userData.name.charAt(0).toUpperCase() + userData.name.slice(1)
+          : "Developer"}
+        !
         <img className="w-8 aspect-square" src={assets.hand_wave} alt="" />
       </h1>
       <h2 className="text-3xl sm:text-5xl font-semibold mb-4">
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,21 +2,51 @@ import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const { userData } = useContext(AppContext);
+  const { userData, setUserData } = useContext(AppContext);
+
+  const logoutHandler = async (req, res) => {
+    try {
+      const { data } = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "/auth/logout"
+      );
+
+      if (data) {
+        toast.success(data.message);
+        setUserData(null);
+      }
+    } catch (error) {
+      console.log("LogoutHandler Error", error);
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 absolute top-0">
       <img src={assets.logo} alt="" className="w-28 sm:w-32" />
       {userData ? (
-        <button
-          onClick={() => navigate("/Dashboard")}
-          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
-        >
-          Dashboard <img src={assets.arrow_icon} alt="" />
+        <button className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+              {!userData?.isAccountVerified && (
+                <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">
+                  Verify email
+                </li>
+              )}
+              <li
+                onClick={logoutHandler}
+                className="py-1 px-2 pr-10 hover:bg-gray-200 cursor-pointer"
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
         </button>
       ) : (
         <button
